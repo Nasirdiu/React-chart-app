adabdf8de82247ff3fa25f7d567e9d2c2ce3aadc
nasirdiu <nasir.joypurhat@gmail.com>
2022-12-07 23:15:34 +0600

    desgin done

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const Chats = () => {
-    return (
-        <div >
-            
+  return (
+    <div className="chats">
+      <div className="userChat">
+        <img src="https://cdn-icons-png.flaticon.com/512/21/21104.png" alt="" />
+        <div className="userChatInfo">
+          <span>Nasir</span>
+          <p>Hello</p>
         </div>
-    );
+      </div>
+      <div className="userChat">
+        <img src="https://cdn-icons-png.flaticon.com/512/21/21104.png" alt="" />
+        <div className="userChatInfo">
+          <span>Nasir</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chats;
--- src/components/Message.js
+++ src/components/Message.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Message = () => {
     return (
-        <div>
-            
+        <div className='message'>
+            <div className="messageInfo">
+                <img src='https://t4.ftcdn.net/jpg/02/24/86/95/360_F_224869519_aRaeLneqALfPNBzg0xxMZXghtvBXkfIA.jpg' alt="" />
+                <span>just now</span>
+            </div>
+            <div className="messageContent">
+                <p>nasir</p>
+                <img src="https://t4.ftcdn.net/jpg/02/24/86/95/360_F_224869519_aRaeLneqALfPNBzg0xxMZXghtvBXkfIA.jpg" alt="" />
+            </div>
         </div>
     );
 };
